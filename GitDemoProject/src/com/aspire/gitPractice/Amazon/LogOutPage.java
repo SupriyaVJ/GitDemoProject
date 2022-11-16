9359c9f91c4449e9cf4f61e78849bea8751d0dd4
User <User@User-PC>
2022-11-16 16:55:19 +0530

    This is my Login and Logout Push Code in master

---
+++ GitDemoProject/src/com/aspire/gitPractice/Amazon/LogOutPage.java
@@ -0,0 +1,9 @@
+package com.aspire.gitPractice.Amazon;
+
+public class LogOutPage 
+{
+	public static void main(String[] args) {
+		
+		System.out.println("This is Logout Page of Amazon");
+	}
+}
---
+++ GitDemoProject/src/com/aspire/gitPractice/Amazon/LoginPage.java
@@ -0,0 +1,10 @@
+package com.aspire.gitPractice.Amazon;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is Login Page for amazon");
+	}
+
+}
