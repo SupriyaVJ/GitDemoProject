44551872ee27147cf8bf11e5f069186ce71ee8cc
User <User@User-PC>
2022-11-16 17:00:13 +0530

    This is my edited code for login page

--- GitDemoProject/src/com/aspire/gitPractice/Amazon/LoginPage.java
+++ GitDemoProject/src/com/aspire/gitPractice/Amazon/LoginPage.java
@@ -5,6 +5,8 @@ public class LoginPage {
 	public static void main(String[] args) {
 		
 		System.out.println("This is Login Page for amazon");
+		System.out.println("Enter user name");
+		System.out.println("Enter pwd");
 	}
 
 }
